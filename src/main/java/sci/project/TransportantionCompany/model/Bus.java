d16f0a962b1bde782585beae3f0181cb48ed9550
george rosescu <george.rosescu@gmail.com>
2020-02-11 19:48:17 +0200

    bus

--- src/main/java/sci/project/TransportantionCompany/model/Bus.java
+++ src/main/java/sci/project/TransportantionCompany/model/Bus.java
@@ -1,4 +1,13 @@
 package sci.project.TransportantionCompany.model;
 
 public class Bus extends Transport {
+    private long id = 0L;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
